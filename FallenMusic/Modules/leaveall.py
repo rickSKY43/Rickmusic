712d597ddf7d1760a463ceffcb4d7ba386a3e658
rickSKY43 <alfianfajar928@gmail.com>
2025-03-09 16:10:22 +0700

    Update leaveall.py

--- FallenMusic/Modules/leaveall.py
+++ FallenMusic/Modules/leaveall.py
@@ -32,7 +32,7 @@ from FallenMusic import ASS_MENTION, SUNAME, app, app2
 
 @app.on_message(filters.command(["leaveall", "assleaveall"]) & filters.user(OWNER_ID))
 async def ass_leaveall(_, message: Message):
-    lear = await message.reply_text(f"» {ASS_MENTION} sᴛᴀʀᴛᴇᴅ ʟᴇᴀᴠɪɴɢ ᴄʜᴀᴛs...")
+    lear = await message.reply_text(f"» {ASS_MENTION} ᴍᴇɴɪɴɢɢᴀʟᴋᴀɴ ɢᴄ ʏᴀɴɢ ᴍᴜᴛᴇ ᴋᴀᴍᴜ...")
     left = 0
     failed = 0
     chats = []
@@ -55,9 +55,9 @@ async def ass_leaveall(_, message: Message):
             failed += 1
     try:
         await lear.edit_text(
-            f"<u>**» {ASS_MENTION} sᴜᴄᴄᴇssғᴜʟʟʏ ʟᴇғᴛ ᴄʜᴀᴛs :**</u>\n\n**ʟᴇғᴛ :** `{left}`\n**ғᴀɪʟᴇᴅ :** `{failed}`"
+            f"<u>**» {ASS_MENTION} ʙᴇʀʜᴀsɪʟ ᴍᴇɴɪɴɢɢᴀʟᴋᴀɴ ɢᴄ ʏɢ ᴍᴜᴛᴇ ᴋᴀᴍᴜ :**</u>\n\n**ʟᴇғᴛ :** `{left}`\n**ғᴀɪʟᴇᴅ :** `{failed}`"
         )
     except:
         await message.reply_text(
-            f"<u>**» {ASS_MENTION} sᴜᴄᴄᴇssғᴜʟʟʏ ʟᴇғᴛ ᴄʜᴀᴛs :**</u>\n\n**ʟᴇғᴛ :** `{left}`\n**ғᴀɪʟᴇᴅ :** `{failed}`"
+            f"<u>**» {ASS_MENTION} ʙᴇʀʜᴀsɪʟ ᴍᴇɴɪɴɢɢᴀʟᴋᴀɴ ɢᴄ ʏɢ ᴍᴜᴛᴇ ᴋᴀᴍᴜ :**</u>\n\n**ʟᴇғᴛ :** `{left}`\n**ғᴀɪʟᴇᴅ :** `{failed}`"
         )
